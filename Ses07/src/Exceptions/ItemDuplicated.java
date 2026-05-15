ccdb58d9e56f250eac6cfe66d03a10cda84e2df9
LeandroLagosZ <leandro.lagos@estudiante.ucsm.edu.pe>
2026-05-15 00:41:17 -0500

    Add files via upload

---
+++ Ses07/src/Exceptions/ItemDuplicated.java
@@ -0,0 +1,13 @@
+package Exceptions;
+
+public class ItemDuplicated extends Exception{
+	public ItemDuplicated(String msg) {
+		super(msg);
+
+	}
+	
+	public ItemDuplicated() {
+		super();
+	}
+
+}
---
+++ Ses07/src/Exceptions/ItemNotFound.java
@@ -0,0 +1,12 @@
+package Exceptions;
+
+public class ItemNotFound extends Exception {
+	public ItemNotFound(String msg) {
+		super(msg);
+
+	}
+	
+	public ItemNotFound() {
+		super();
+	}
+}
